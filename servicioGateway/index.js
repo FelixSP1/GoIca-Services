8760820e8e6c93ca9aca0f38881797a4f87c19db
FelixSP1 <salasfelix107@gmail.com>
2025-12-01 15:05:46 -0500

    masmodificaciones

--- servicioGateway/index.js
+++ servicioGateway/index.js
@@ -14,6 +14,7 @@ app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
 
 // --- SOLUCIÓN PARA EL ERROR OPTIONS/PREFLIGHT ---
